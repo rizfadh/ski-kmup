bcd02ca5dbedca4c0f4b28dac6354460f5b47c05
rizfadh <kikimuhammad69@gmail.com>
2024-04-21 00:31:48 +0700

    delete Account model

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -2,6 +2,19 @@ import { auth } from "@/auth";
 import LogoutButton from "@/components/LogoutButton";
 import { getUserById } from "@/lib/user";
 
+const greetings = (name: string) => {
+  const currentHour = new Date().getHours();
+  if (currentHour < 12) {
+    return `Selamat pagi ${name}`;
+  } else if (currentHour < 15) {
+    return `Selamat siang ${name}`;
+  } else if (currentHour < 18) {
+    return `Selamat sore ${name}`;
+  } else {
+    return `Selamat malam ${name}`;
+  }
+};
+
 export default async function DashboardPage() {
   const session = await auth();
 
@@ -13,8 +26,8 @@ export default async function DashboardPage() {
     <div className="container grid grid-cols-1 gap-8">
       <p className="text-5xl font-black">Dashboard</p>
       <LogoutButton />
-      <div className="mb-8">
-        <p>Halo selamat datang {session.user.name}</p>
+      <div>
+        <p>{greetings(session.user.name as string)}</p>
         <p>Ini data kamu yang ada di database</p>
 
         <pre className="mt-2 overflow-x-auto rounded-md bg-slate-950 p-4 ">
@@ -23,6 +36,9 @@ export default async function DashboardPage() {
           </code>
         </pre>
       </div>
+      <p className="mb-8 text-center text-sm text-muted-foreground">
+        Webnya masih blom jadi
+      </p>
     </div>
   );
 }
